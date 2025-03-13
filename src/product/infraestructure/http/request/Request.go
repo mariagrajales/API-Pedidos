47e4aae8346be838fc9b4502af7ea1565399b779
Eduardo Trujillo <129311642+Lalo64GG@users.noreply.github.com>
2025-03-12 23:31:08 -0600

    remove product application use cases, domain entities, ports, and HTTP controllers

--- src/product/infraestructure/http/request/Request.go
+++
@@ -1,8 +0,0 @@
-package request
-
-type ProductRequest struct {
-	Name        string  `json:"name" validate:"required"`
-	Description string  `json:"description" validate:"required"`
-	Price       float64 `json:"price" validate:"required"`
-	Stock       int     `json:"stock" validate:"required"`
-}
